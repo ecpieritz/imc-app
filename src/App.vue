1bc47d26c0880647cd0f211027f6d6562d742a7a
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-06-22 13:53:55 -0300

    Added inputs and button

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
   <div id="app">
     <h1>Cálculo do IMC</h1>
     <h2>Digite seu peso e altura para calcular o IMC</h2>
+    <span class="p-float-label">
+      <InputText id="input-weight" type="text" v-model="weight" />
+      <label for="input-weight">Peso</label>
+    </span>
 
+    <span class="p-float-label">
+      <InputText id="input-height" type="text" v-model="height" />
+      <label for="input-height">Altura</label>
+    </span>
+
+    <Button label="Calcular" />
   </div>
 </template>
 
 <script>
-
-
 export default {
-
-}
+  data() {
+    return {
+      height: null,
+      weight: null,
+    };
+  },
+};
 </script>
 
 <style>
-  
 </style>
